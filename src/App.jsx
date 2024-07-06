924738a1d246502386a1789b1615e97054a05a34
MajdTamimi <majd2004tamimi@gmail.com>
2024-07-07 00:16:23 +0300

    navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from 'react'
-import Card from './components/Card';
-
+import Navbar from "./components/Navbar";
+import Recipes from "./Pages/Recipes";
 function App() {
-  const [recipes, setRecipes] = useState([]);
-  const BASE_URL = 'https://dummyjson.com'
-
-  useEffect(() => {
-    fetch(`${BASE_URL}/recipes`,
-      {
-        method: "GET"
-      })
-      .then(res => res.json())
-      .then(data => {
-        setRecipes(data.recipes)
-      })
-  }, [])
-
   return (
-    <div className='flex justify-center gap-10 flex-wrap'>
-      {
-        recipes.map((recipe) => {
-          return (
-            <Card key={recipe.id} recipe={recipe} />
-          )
-        })
-      }
+    <div> 
+      <Navbar />
+      <Recipes />
     </div>
   )
 }
