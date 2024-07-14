f70078789036ea76128cdd89bdf52f50708ddbaa
MajdTamimi <majd2004tamimi@gmail.com>
2024-07-14 21:28:54 +0300

    login page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Details from "./Pages/Details";
 import Home from "./Pages/Home";
 import Recipes from "./Pages/Recipes";
 import NotFound from "./Pages/NotFound";
+import Login from "./Pages/Login";
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/recipes" element={<Recipes />} />
           <Route path="/recipes/details/:id" element={<Details />} />
           <Route path="*" element={<NotFound />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,6 +40,9 @@ const Navbar = () => {
           </div>
           <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#151C2A] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
+              <a href="/login" className="block py-2 px-3 text-lg text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#E45900] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</a>
+            </li>
+            <li>
               <a href="/" className="block py-2 px-3 text-lg text-white bg-blue-700 rounded md:bg-transparent md:hover:text-[#E45900] md:text-white md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
             </li>
             <li>
