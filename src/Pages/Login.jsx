0360c415e12f6910a282d4cf81aac205d6be91dc
MajdTamimi <majd2004tamimi@gmail.com>
2024-07-18 00:36:42 +0300

    login functionality

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,36 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import background from "../assets/images/Com.png"
-import {BiUser} from "react-icons/bi"
-import {AiOutlineUnlock} from "react-icons/ai"
+import { BiUser } from "react-icons/bi"
+import { AiOutlineUnlock } from "react-icons/ai"
+import { useState } from "react"
 
 const Login = () => {
+
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    function login() {
+        fetch('https://dummyjson.com/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username: userName,
+                password: password,
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                localStorage.setItem('token', data.token);
+                console.log("login successful", data.token);
+                navigate("/")
+            });
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        login();
+    };
+    
     return (
         <>
             <div className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: `url(${background})` }}>
@@ -11,14 +38,14 @@ const Login = () => {
                 <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
                     <div className="bg-[#f3cab02a] border border-[#ff63022a] rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative ">
                         <h1 className="text-4xl text-white font-bold text-center mb-6">Login</h1>
-                        <form action="">
+                        <form action="" onSubmit={handleSubmit} >
                             <div className="relative my-4 ">
-                                <input className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-[#E45900] focus:outline-none focus:ring-0 focus:text-white focus:border-[#E45900] peer" type="email" placeholder="" />
+                                <input type="text" placeholder="" value={userName} onChange={(e) => setUserName(e.target.value)} required className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-[#E45900] focus:outline-none focus:ring-0 focus:text-white focus:border-[#E45900] peer" />
                                 <label htmlFor="" className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#E45900] peer-focus:dark:text-[#E45900] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-90 peer-focus:-translate-y-6">Your Email</label>
                                 <BiUser className="absolute top-4 right-4" />
                             </div>
                             <div className="relative my-4 ">
-                                <input className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-[#E45900] focus:outline-none focus:ring-0 focus:text-white focus:border-[#E45900] peer" type="password" placeholder="" />
+                                <input type="password" placeholder="" value={password} onChange={(e) => setPassword(e.target.value)} required className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-[#E45900] focus:outline-none focus:ring-0 focus:text-white focus:border-[#E45900] peer" />
                                 <label htmlFor="" className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#E45900] peer-focus:dark:text-[#E45900] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-90 peer-focus:-translate-y-6">Your Password</label>
                                 <AiOutlineUnlock className="absolute top-4 right-4" />
                             </div>
